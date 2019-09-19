f6988b93270e77e3a38e69d7c60a7128279d0134
tomasdem <tomasdembelli@gmail.com>
2019-09-19 23:28:20 +0100

    Validation: No known face encoding raises value error. Added: Verbose initialisation.

--- find_my_pictures.py
+++ find_my_pictures.py
@@ -9,11 +9,13 @@ from datetime import datetime
 from shutil import copy, move
 
 
+
 class FindMyPictures():
     """Find pictures of interest within a folder of pictures."""
-    def __init__(self, input_sample=None, input_stack=None, output=None):
+    def __init__(self, input_sample=None, input_stack=None, output=None, verbose=False):
         """Explain arguments here."""
         # Initializing folders
+        self.initiate_verbose = verbose
         cwd = os.getcwd()
         if input_sample:
             self.input_sample = input_sample
@@ -27,10 +29,20 @@ class FindMyPictures():
             self.output = output
         else:
             self.output = os.path.join(cwd, 'output')
+        if self.initiate_verbose:
+            print(f'Images in {self.input_sample} will be used for training.')
+            print(f'Images in {self.input_stack} will be analyzed to identify person of interest.')
+            print(f'Images with positive matches will be stored in {self.output}.')
             
-    def analyze_poi(self):
+    def analyze_poi(self, accuracy=None):
         """Explain"""    
         print("Analyzing the person of interest's face.")
+        if accuracy == 'medium':
+            num_jitters = 10
+        elif accuracy == 'high':
+            num_jitters = 30
+        elif not accuracy:
+            num_jitters = 1
         start = time.time()
         self.sample_img = self.validate_image_folder(self.input_sample)
         if len(self.sample_img) < 1:
@@ -40,7 +52,7 @@ class FindMyPictures():
             self.known_enc = []
             for img in self.sample_img:
                 known_img = fr.load_image_file(img)
-                known_enc = fr.face_encodings(known_img, num_jitters=10)
+                known_enc = fr.face_encodings(known_img, num_jitters=num_jitters)
                 if len(known_enc) > 1:
                     print(f"Sample image {img} (training data) can't contain more than 1 face.")
                 else:
@@ -48,6 +60,8 @@ class FindMyPictures():
         end = time.time()
         process_time = end - start
         print(f'Finished within {process_time} seconds.')
+        if len(self.known_enc) < 1:
+            print('Person of interest face recognition is unsuccessfull.')
         
             
     def validate_image_folder(self, folder):
@@ -68,6 +82,8 @@ class FindMyPictures():
     
     def analyze_img(self, img):
             """Return True if there is a positive match."""
+            if len(self.known_enc) < 1:
+                raise ValueError('There is no trained data for the person of interest.')
             img_load = fr.load_image_file(img)
             img_enc = fr.face_encodings(img_load)
             result = False
@@ -83,7 +99,7 @@ class FindMyPictures():
                             move(img, self.positive_folder)
                         self.match_count.value += 1
                         break
-            if self.verbose:
+            if self.find_verbose:
                 status = [f'{img.split("/")[-1]}\t']
                 if len(img_enc) > 0:
                     status.append(f'{len(img_enc)} face detected')
@@ -96,10 +112,12 @@ class FindMyPictures():
     
     def find_pictures(self, verbose=False, copy=True):
         """Looks for the person of interest in the input stack images."""
+        if len(self.known_enc) < 1:
+            raise ValueError('There is no trained data for the person of interest.')
         self.stack_images = self.validate_image_folder(self.input_stack)
         if len(self.input_stack) < 1:
             raise ValueError(f'There is no image in the {self.input_stack} folder.')
-        self.verbose = verbose
+        self.find_verbose = verbose
         positive_folder_name = ''.join(['Positive_Match', '_', datetime.now().strftime('%Y_%m_%d_%H-%M')])
         positive_folder = os.path.join(self.output, positive_folder_name)
         os.mkdir(positive_folder)
@@ -119,4 +137,4 @@ class FindMyPictures():
                 print(f'Finished within {self.process_time} seconds.')
                 if self.match_count.value > 0:
                     print(f'Person of interest is recognised in {self.match_count.value} images.')
-                    print(f'Positive matches have been stored in {self.positive_folder}')
+                    print(f'Positive matches have been stored in {self.positive_folder}.')
